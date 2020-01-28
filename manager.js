72052ac0692e00d494c4e28023885cecb2f0ba35
lmoody <l.moody@continuumonline.com>
2020-01-28 16:30:27 -0700

    added functionality to update an employee's role

--- manager.js
+++ manager.js
@@ -30,6 +30,7 @@ const initialize = () => {
                     , "View All Employees by Department"
                     , "View All Employees by Role"
                     , "Add an Employee"
+                    , "Update an Employee's Role"
                     , "Remove an Employee"
                     , "Exit"
                 ]
@@ -47,6 +48,9 @@ const initialize = () => {
                 break;
                 case "Add an Employee":
                     addEmployee();
+                break;
+                case "Update an Employee's Role":
+                    updateEmployee();
                 break;
                 case "Remove an Employee":
                     deleteEmployee();
@@ -178,47 +182,36 @@ const addEmployee = () => {
                 case "CEO":
                     var roleID = 1;
                 break;
-            
                 case "COO":
                     var roleID = 2;
                 break;
-            
                 case "CTO":
                     var roleID = 3;
                 break;
-            
                 case "CFO":
                     var roleID = 4;
                 break;
-            
                 case "Sales Team Lead":
                     var roleID = 5;
                 break;
-            
                 case "Salesperson":
                     var roleID = 6;
                 break;
-            
                 case "Controller":
                     var roleID = 7;
                 break;
-            
                 case "Accountant":
                     var roleID = 8;
                 break;
-            
                 case "Legal Team Lead":
                     var roleID = 9;
                 break;
-            
                 case "Lawyer":
                     var roleID = 10;
                 break;
-            
                 case "Engineer Team Lead":
                     var roleID = 11;
                 break;
-            
                 case "Software Engineer":
                     var roleID = 12;
                 break;
@@ -238,7 +231,109 @@ const addEmployee = () => {
                     initialize();
                 });
         });
-}
+};
+
+const updateEmployee = () => {
+    const query = "SELECT CONCAT(first_name, ' ', last_name) as name FROM employee;"
+    connection.query(
+        query
+        , (err, res) => {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        type: "list"
+                        , message: "Which Employee would you like to update?"
+                        , name: "selectedEmp"
+                        , choices: () => {
+                            var choiceArray = [];
+                            for (const item of res) {
+                                choiceArray.push(item.name);
+                            }
+                            return choiceArray;
+                        }
+                    },
+                    {
+                        name: "choice"
+                        , type: "list"
+                        , message: "What is this employee's title?"
+                        , choices: [
+                            "CEO"
+                            , "COO"
+                            , "CTO"
+                            , "CFO"
+                            , "Sales Team Lead"
+                            , "Salesperson"
+                            , "Controller"
+                            , "Accountant"
+                            , "Legal Team Lead"
+                            , "Lawyer"
+                            , "Engineer Team Lead"
+                            , "Software Engineer"
+                        ]
+                    }
+                ]).then(data => {
+                    switch (data.choice) {
+                        case "CEO":
+                            var roleID = 1;
+                            break;
+                        case "COO":
+                            var roleID = 2;
+                            break;
+                        case "CTO":
+                            var roleID = 3;
+                            break;
+                        case "CFO":
+                            var roleID = 4;
+                            break;
+                        case "Sales Team Lead":
+                            var roleID = 5;
+                            break;
+                        case "Salesperson":
+                            var roleID = 6;
+                            break;
+                        case "Controller":
+                            var roleID = 7;
+                            break;
+                        case "Accountant":
+                            var roleID = 8;
+                            break;
+                        case "Legal Team Lead":
+                            var roleID = 9;
+                            break;
+                        case "Lawyer":
+                            var roleID = 10;
+                            break;
+                        case "Engineer Team Lead":
+                            var roleID = 11;
+                            break;
+                        case "Software Engineer":
+                            var roleID = 12;
+                            break;
+                    }
+                    const emp = data.selectedEmp.split(" ");
+                    const query = "UPDATE employee SET ? WHERE ? AND ?";
+                    connection.query(
+                        query
+                        , [
+                            {
+                                role_id: roleID
+                            },
+                            {
+                                first_name: emp[0]
+                            },
+                            {
+                                last_name: emp[1]
+                            }
+                        ]
+                        , err => {
+                            if (err) throw err;
+                            console.log("Employee Successfully Updated!");
+                            initialize();
+                        });
+                });
+        });
+};
 
 const deleteEmployee = () => {
     const query = "SELECT CONCAT(first_name, ' ', last_name) as name FROM employee;"
